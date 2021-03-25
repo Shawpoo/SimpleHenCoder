bd76480afa42feca0a30720d8759ed3a8a793350
Shawpoo <994227083@qq.com>
2021-03-25 14:32:31 +0800

    [feature] 增加sp转px方法

--- app/src/main/java/com/shawpoo/simplehencoder/app/ext/Ext.kt
+++ app/src/main/java/com/shawpoo/simplehencoder/app/ext/Ext.kt
@@ -6,12 +6,19 @@ import android.util.TypedValue
 /**
  * @author: wuxiaopeng
  * @date: 2021/3/20
- * @desc:
+ * @desc: dp、sp转px
  */
 // xx.dp 5f.dp 表示5dp所对应的像素值
 val Float.dp
     get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this,
         Resources.getSystem().displayMetrics)
 
+val Float.sp
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this,
+        Resources.getSystem().displayMetrics)
+
 val Int.dp
     get() = this.toFloat().dp
+
+val Int.sp
+    get() = this.toFloat().sp
