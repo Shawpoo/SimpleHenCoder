8d80db718cf8a3cda79ae976da8216ec25325cb2
Shawpoo <994227083@qq.com>
2021-03-26 12:42:09 +0800

    [feature] 提交ConstraintLayout的基础用法示例

---
+++ app/src/main/java/com/shawpoo/simplehencoder/app/layout/sample/ConstraintLayoutSampleActivity.kt
@@ -0,0 +1,20 @@
+package com.shawpoo.simplehencoder.app.layout.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shawpoo.simplehencoder.app.R
+
+/**
+ * @author: wuxiaopeng
+ * @date: 2021/3/25
+ * @desc: ConstraintLayout示例
+ * https://www.jianshu.com/p/17ec9bd6ca8a
+ */
+class ConstraintLayoutSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraintlayout_sample)
+    }
+
+}
